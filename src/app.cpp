fddff33aa59de3f4cf99417395f2a487fd6c7c3b
Chimichan-dorid <jacobymartin1@icloud.com>
2025-03-24 19:01:38 -0700

    Linker broken. Issues with the GLAD files

--- src/app.cpp
+++ src/app.cpp
@@ -1,13 +1,47 @@
+
+#include <GLFW/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <glad/glad.h>
-#include <glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 
 int main() {
-	
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My first Window!!!", NULL, NULL);
+	
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << "\n";
+		glfwTerminate();
+		return 0;
+	}
+	glfwMakeContextCurrent(window); // Current context is the window pointer
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << "\n";
+		return 0;
+	}
+
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+};
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
